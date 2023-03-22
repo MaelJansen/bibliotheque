28b5290a99fe8c7bda5dc424306cb162e3687de8
LEVEQUE <loic.leveque@etu.u-bordeaux.fr>
2023-03-22 14:22:39 +0100

    Added access to randomUserApi

--- database_utils/bdFilling.py
+++ database_utils/bdFilling.py
@@ -16,18 +16,30 @@ def remplissageBD():
     try:
         cnx = mysql.connector.connect(user='root', password='root', host=''
                                         , database='projet')
-        
+        users = creationUsers()
+        for user in users:
+            print(user)
     except mysql.connector.Error as err:
         throw(err)
 
 def creationUsers():
+    import requests
+    url = "https://randomuser.me/api/"
+    dataParams = {'nat' : 'fr', 'results' : 50,'inc':'name,email,picture,login'}
+    answer = requests.get(url, params=dataParams)
+    for i in range(5):
+        print(answer.json()['results'][i]['name']['first'])
+        print(answer.json()['results'][i]['name']['last'])
+        print(answer.json()['results'][i]['email'])
+        print(answer.json()['results'][i]['picture']['large'])
+        print(answer.json()['results'][i]['login']['password'])
     return None
 
 def recuperationLivresApi():
     import requests
-    url = "https://www.googleapis.com/books/v1/volumes?q=''"
-    params = {'limit': 10, 'offset': 0}
-    answer = requests.get(url, params=params)
-    print(answer.json())
+    url = "https://www.googleapis.com/books/v1/volumes?"
+    dataParams = {'maxResults': 40, 'q': 'a'}
+    answer = requests.get(url, params=dataParams)
+    print(len(answer.json()['items']))
 
-recuperationLivresApi()
+creationUsers()
