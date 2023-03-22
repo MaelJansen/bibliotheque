6d4596dfe3c9316b88cf69e7d8a91b6a7ce9a543
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-22 18:02:01 +0100

    Work at the end of day 1

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import SearchBar from "./components/searchbar";
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-iut-green font-saira'>Hello</h1>
+      <SearchBar />
     </div>
   );
 }
