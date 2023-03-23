f9bc34c829df0e44a6021078b6540a2a9da9e717
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-23 17:10:36 +0100

    Back home on error

--- front/src/routes/error-page.js
+++ front/src/routes/error-page.js
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -9,6 +9,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page" className="w-screen h-screen flex flex-col items-center bg-black">
       <img src={imgPath} alt={error.status + " : " + error.statusText}></img>
+      <Link className="transition-colors text-white ring-1 ring-white p-2 hover:bg-white hover:text-black" to={`/`}>Return home</Link>
     </div>
   );
 }
