d332aae30d31e8bd7670dd1a7a051beaf4e6633c
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-23 09:15:22 +0100

    Add background image

--- front/src/components/searchbar.js
+++ front/src/components/searchbar.js
@@ -3,16 +3,16 @@ function SearchBar(props) {
     const [query, setQuery] = useState("");
 
     return (
-        <div className='w-full bg-slate-500 p-10 flex flex-col align-middle justify-center'>
+        <div className='w-full h-fit bg-header-img p-16 flex flex-col align-middle justify-center'>
             <form className='flex flex-row justify-center'>
                 <label for="default-search" class="text-sm font-medium text-gray-900 sr-only">Search</label>
-                <div class="relative">
+                <div class="relative w-3/5 sm:md:w-4/5 ">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
                     <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 focus:ring-iut-green focus:border-blue-500" placeholder="Search" required />
                 </div>
-                <button type="submit" class="p-2.5 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                <button type="submit" class="p-2.5 text-sm font-medium text-white bg-iut-green border border-iut-green hover:bg-iut-hover-green focus:ring-4 focus:outline-none focus:ring-iut-green">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     <span class="sr-only">Search</span>
                 </button>
