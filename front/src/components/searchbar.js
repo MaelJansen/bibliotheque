4581205f637e9744b3e6bc4326a70811c5735c48
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-23 15:04:50 +0100

    Navigation in suggestion

--- front/src/components/searchbar.js
+++ front/src/components/searchbar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 function SearchBar({ query, onChangeQuery, completion }) {
 
+    const searchRef = useRef(null);
+
     const handleChange = (newQuery) => {
+        searchRef.current.focus();
         onChangeQuery(newQuery);
-    };
+    }
 
     return (
         <>
@@ -15,6 +18,7 @@ function SearchBar({ query, onChangeQuery, completion }) {
                             <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         </div>
                         <input type="search"
+                            ref={searchRef}
                             id="default-search"
                             value={query}
                             class="block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 focus:border-iut-hover-green"
@@ -25,9 +29,13 @@ function SearchBar({ query, onChangeQuery, completion }) {
                         <div className='absolute w-full border border-iut-green'>
                             <ol className='bg-white w-full'>
                                 {completion.map((elem) => (
-                                    <li key={elem} 
-                                    className="p-2, border border-b-gray-200 w-full">
-                                        <button className='w-full text-left pl-2 overflow-x-clip'>
+                                    <li key={elem}
+                                        className="border border-b-gray-200 w-full">
+                                        <button
+                                            type='button'
+                                            className='w-full text-left pl-2 py-2 overflow-x-clip hover:text-iut-hover-green hover:ring-1 hover:ring-iut-hover-green focus:text-iut-hover-green focus:ring-1 focus:ring-iut-hover-green'
+                                            value={elem}
+                                            onClick={e => handleChange(e.target.value)}>
                                             {elem}
                                         </button>
                                     </li>
