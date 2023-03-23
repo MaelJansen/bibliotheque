ea247e02806a61c9c0c699dbbf71fdbace417c5b
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-23 11:08:46 +0100

    SearchBar content stock in the app

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,26 @@
 import SearchBar from "./components/searchbar";
+import React, {useEffect, useState} from "react";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  const onChangeQuery = (newQuery) => {
+    setQuery(newQuery);
+  }
+
+  useEffect(() => {
+    // Detect the change
+    console.log(query)
+  })
+
   return (
     <div className="App">
       <h1 className='text-iut-green font-saira'>Hello</h1>
-      <SearchBar />
+      <SearchBar 
+      query={query}
+      onChangeQuery={onChangeQuery}/>
+      <p>You search : {query}</p>
     </div>
   );
 }
--- front/src/components/searchbar.js
+++ front/src/components/searchbar.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
-function SearchBar(props) {
-    const [query, setQuery] = useState("");
+import React from 'react';
+function SearchBar({query, onChangeQuery}) {
+    
+    const handleChange = (newQuery) => {
+        onChangeQuery(newQuery);
+      };
 
     return (
         <div className='w-full h-fit bg-header-img p-16 flex flex-col align-middle justify-center'>
@@ -10,7 +13,13 @@ function SearchBar(props) {
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
-                    <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 focus:ring-iut-green focus:border-iut-hover-green" placeholder="Search" required />
+                    <input type="search"
+                        id="default-search"
+                        value={query}
+                        class="block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 focus:ring-iut-green focus:border-iut-hover-green"
+                        placeholder="Search"
+                        onChange={e => handleChange(e.target.value)}
+                        required />
                 </div>
                 <button type="submit" class="p-2.5 text-sm font-medium text-white bg-iut-green border border-iut-green hover:bg-iut-hover-green focus:ring-4 focus:outline-none focus:ring-iut-green">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
