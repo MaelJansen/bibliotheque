b861fb2c1834adeb190bd9a735df326cd8f21d2a
Mael Jansen <maeljansen2003@gmail.com>
2023-03-24 09:41:34 +0100

    Modified header (hoverand file position)

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -1,6 +1,6 @@
 import SearchBar from "../components/searchbar";
 import React, {useState } from "react";
-import Header from "./Header";
+import Header from "../components/Header";
 
 function Home() {
 
