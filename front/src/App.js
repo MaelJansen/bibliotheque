55d3b31a47a0e73c2c13263a3517c42741a8fda3
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-27 16:18:38 +0200

    Routes without error handling

--- front/src/App.js
+++ front/src/App.js
@@ -7,13 +7,11 @@ import Results from './routes/Results';
 function App() {
     return (
         <Routes>
-            <Route path="/" Component={Main}>
+            <Route exact path="/" Component={Main}>
                 <Route index Component={Home} />
                 <Route path="books" Component={Results} />
                 <Route path="books/:id" Component={Home} />
-                <Route path="*" element={<ErrorPage />} />
             </Route>
-            <Route path="*" element={<ErrorPage />} />
         </Routes>
     );
 }
