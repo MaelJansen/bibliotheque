5c147255b7fcf227df9e70bd091f403f72148b76
LEVEQUE <loic.leveque@etu.u-bordeaux.fr>
2023-03-28 14:49:59 +0200

    refactor: :memo: Added comments
    added comments + checking if user exist

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -32,10 +32,16 @@ class UserController extends AbstractController
         return $this->json($responseData, 200, [], ['groups' => 'last_books']);
     }
 
+    // Get all friends from one user
     #[Route('/{id}/friends', methods: ['GET'])]
     public function getUserFriends(int $id, UserRepository $userRepository)
     {
+        // Get the user with the specified id
         $user = $userRepository->getOneUser($id);
+        if (!$user) {
+            return $this->json(['message' => 'User not found'], 400);
+        }
+        // Get the friends of the user
         $friends = $user->getUSRFollowedUsers();
         return $this->json($friends, 200, [], ['groups' => 'user_infos']);
     }
