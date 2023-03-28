27cdc3ad05f9689e7e965b05eb1e32ad1f052f53
Mael Jansen <maeljansen2003@gmail.com>
2023-03-28 14:40:16 +0200

    fix pipeline error

--- back/src/Controller/BookController.php
+++ back/src/Controller/BookController.php
@@ -14,15 +14,15 @@ class BookController extends AbstractController
     #[View(serializerGroups: ['preview'])]
     #[Route('/', name: 'app_book', methods:['GET'])]
     public function index(BookRepository $repository)
-    {   
+    {
         if (!isset($_GET['q']) || !isset($_GET['page']) || !isset($_GET['result'])) {
             throw new HttpException(400, "Missing query parameter");
         }
-        $query = $_GET['q']?$_GET['q']:"";
-        $page = $_GET['page']?$_GET['page']:1;
-        $NbResult = $_GET['result']?$_GET['result']:10;
+        $query = $_GET['q'] ? $_GET['q'] : "";
+        $page = $_GET['page'] ? $_GET['page'] : 1;
+        $NbResult = $_GET['result'] ? $_GET['result'] : 10;
 
-        $res = array_slice($repository->findByAuthor($query), $page-1, $NbResult);
+        $res = array_slice($repository->findByAuthor($query), $page - 1, $NbResult);
         return $res;
     }
 
