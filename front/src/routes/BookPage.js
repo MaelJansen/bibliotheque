fd55ca51c588a99868de93dc1f345e9fc7e859fa
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-28 15:12:28 +0200

    Better responsive

--- front/src/routes/BookPage.js
+++ front/src/routes/BookPage.js
@@ -12,7 +12,11 @@ function BookPage() {
         axios
             .get(serverQuery)
             .then((response) => {
-                setBookData(response.data[0]);
+                if (response.data[0]) {
+                    setBookData(response.data[0]);
+                } else {
+                    setBookData({"error": true});
+                }
             })
     }
 
@@ -23,17 +27,19 @@ function BookPage() {
     return (
         <div className="m-5">
             <p className="text-iut-green text-lg mb-3">{bookData.BOOName}</p>
-            <div className="flex flex-col w-full overflow-hidden max-h-[500px] md:flex-row" >
-                <div className="group relative mb-3 w-full md:w-3/5 h-full overflow-hidden md:mr-3 rounded-md">
-                    <img src={bookData.BOOLinkImg} alt={bookData.BOOName} className="object-cover w-full h-fit group-hover:scale-110"/>
+            <div className="flex flex-col w-full h-full overflow-hidden md:flex-row" >
+                <div className="mb-3 md:mb-0 w-full md:w-3/5 max-h-[600px] overflow-hidden md:mr-3 rounded-md">
+                    <img src={bookData.BOOLinkImg || "https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"} 
+                    alt={bookData.BOOName} 
+                    className="object-cover w-full h-fit" />
                 </div>
-                <div className="bg-gray-200 w-full md:w-2/5 grid grid-cols-2 p-2 rounded-md">
-                    <p>Langue : <span className="text-iut-green h-min p-1">{ bookData.BOOLanguages ? bookData.BOOLanguages[0].LANName : "Non renseigné"}</span></p>
-                    <p>Nombre de pages : <span className="text-iut-green h-min p-1">{ bookData.BOONbPages | "Non renseigné"}</span></p>
-                    <p>Catégorie : <span className="text-iut-green h-min p-1">{ bookData.BOOCategory ? bookData.BOOCategory.CATName : "Non renseigné"}</span></p>
-                    <p>Editeur : <span className="text-iut-green h-min p-1">{ bookData.BOOEditor ? bookData.BOOEditor.EDIName : "Non renseigné"}</span></p>
-                    <p className="text-xl col-span-2 font-semi-bold">Resume</p>
-                    <p className="col-span-2">{bookData.BOOSummary ? bookData.BOOSummary : "Non renseigné"}</p>
+                <div className="bg-gray-200 w-full md:w-2/5 grid grid-cols-1 md:grid-cols-2 p-2 rounded-md">
+                    <p>Langue : <span className="text-iut-green h-min p-2">{ bookData.BOOLanguages ? bookData.BOOLanguages[0].LANName : "Non renseigné"}</span></p>
+                    <p>Nombre de pages : <span className="text-iut-green h-min p-2">{bookData.BOONbPages || "Non renseigné"}</span></p>
+                    <p>Catégorie : <span className="text-iut-green h-min p-2">{bookData.BOOCategory ? bookData.BOOCategory.CATName : "Non renseigné"}</span></p>
+                    <p>Editeur : <span className="text-iut-green h-min p-2">{bookData.BOOEditor ? bookData.BOOEditor.EDIName : "Non renseigné"}</span></p>
+                    <p className="text-xl md:col-span-2 font-semi-bold p-2">Resume</p>
+                    <p className="md:col-span-2 p-2">{bookData.BOOSummary || "Non renseigné"}</p>
                 </div>
             </div>
         </div>
