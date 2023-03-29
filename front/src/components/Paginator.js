e786ba69b429182c019ed35b87f23f6deba36a5d
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-29 17:40:44 +0200

    Resolve merge conflict

---
+++ front/src/components/Paginator.js
@@ -0,0 +1,36 @@
+import { useSearchParams, Link, useNavigate, useLocation } from 'react-router-dom';
+
+function Paginator({ nbResult }) {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [searchParams] = useSearchParams();
+
+    const page = searchParams.get('page') ? parseInt(searchParams.get('page')) : 1;
+    const resultByPage = searchParams.get('result') ? searchParams.get('result') : 10;
+    const nbPage = Math.max(Math.ceil(nbResult / resultByPage), 1);
+    const search = searchParams.get('q') ? `&q=${searchParams.get('q')}` : "";
+
+    if (page > nbPage) {
+        navigate(`${location.pathname}?page=${nbPage}&result=${resultByPage}${search}`);
+    }
+
+    else if (nbResult > 0) {
+        const previous = page <= 1 ? "" : "<<";
+        const next = page >= nbPage ? "" : ">>"
+
+        return (
+            <div className="paginator p-2">
+                <p>Showing <span className='text-iut-green'>{page >= nbPage ? nbResult % resultByPage : resultByPage}</span> of <span className='text-iut-green'>{nbResult}</span></p>
+                <div className='flex flex-row space-x-5 justify-center'>
+                    <Link to={`${location.pathname}?page=${page - 1}&result=${resultByPage}${search}`} replace={true} className="hover:text-iut-green">{previous}</Link>
+                    <p>{page} / {nbPage}</p>
+                    <Link to={`${location.pathname}?page=${page + 1}&result=${resultByPage}${search}`} replace={true} className="hover:text-iut-green">{next}</Link>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Paginator;
--- front/src/components/bookPrev.js
+++ front/src/components/bookPrev.js
@@ -6,9 +6,9 @@ function BookPrev({ data }) {
     return (
         <div id="preview" className="h-64 w-full flex flex-col">
             <div className="group overflow-hidden cursor-pointer">
-                <Link to={`/books/${data.id}`}
-                    className="w-full h-fit overflow-hidden">
-                    <img src={data.img}
+                <Link to={`/books/${data.id}`} 
+                className="w-full h-fit overflow-hidden">
+                    <img src={data.img || "https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"}
                         className="object-cover object-top w-full h-full ease-in-out delay-100 transition-transform  group-hover:scale-110 group-focus:scale-110"
                         alt={data.title} />
                 </Link>
