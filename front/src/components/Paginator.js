05872ee459526cb32866d97b04d3e02729f4ffe1
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-29 15:47:35 +0200

    Handle to many page

--- front/src/components/Paginator.js
+++ front/src/components/Paginator.js
@@ -1,26 +1,34 @@
-import { useSearchParams, Link } from 'react-router-dom';
+import { useSearchParams, Link, useNavigate } from 'react-router-dom';
 
 function Paginator({ nbResult }) {
 
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
+
     const page = searchParams.get('page') ? parseInt(searchParams.get('page')) : 1;
     const resultByPage = searchParams.get('result') ? searchParams.get('result') : 10;
     const nbPage = Math.ceil(nbResult / resultByPage);
     const search = searchParams.get('q');
 
-    const previous = page <=1 ? "" : "<<";
-    const next = page >= nbPage ? "" : ">>"
+    if (page > nbPage) {
+        navigate(`/books?q=${search}&page=${nbPage}&result=${resultByPage}`);
+    }
 
-    return (
-        <div className="paginator ">
-            <p>Showing <span className='text-iut-green'>{page >= nbPage ? nbResult % resultByPage : resultByPage}</span> of <span className='text-iut-green'>{nbResult}</span></p>
-            <div className='flex flex-row space-x-5 justify-center'>
-                <Link to={`/books?q=${search}&page=${page-1}&result=${resultByPage}`}>{previous}</Link>
-                <p>{page} / {nbPage}</p>
-                <Link to={`/books?q=${search}&page=${page+1}&result=${resultByPage}`}>{next}</Link>
+    else if (nbResult > 0) {
+        const previous = page <= 1 ? "" : "<<";
+        const next = page >= nbPage ? "" : ">>"
+
+        return (
+            <div className="paginator p-2">
+                <p>Showing <span className='text-iut-green'>{page >= nbPage ? nbResult % resultByPage : resultByPage}</span> of <span className='text-iut-green'>{nbResult}</span></p>
+                <div className='flex flex-row space-x-5 justify-center'>
+                    <Link to={`/books?q=${search}&page=${page - 1}&result=${resultByPage}`} replace={true}>{previous}</Link>
+                    <p>{page} / {nbPage}</p>
+                    <Link to={`/books?q=${search}&page=${page + 1}&result=${resultByPage}`} replace={true}>{next}</Link>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 
 }
 
