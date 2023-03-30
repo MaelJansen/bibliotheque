2dd5e1a89430282669b5b69d234a52c2fb1d91cb
Noyer <emilien.noyer@etu.u-bordeaux.fr>
2023-03-30 19:45:15 +0200

    login now works git add *git add * (logout doesn't)

--- back/src/Controller/ApiLoginController.php
+++ back/src/Controller/ApiLoginController.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use OpenApi\Attributes as OA;
+use OpenApi\Annotations as OAn;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,29 +27,33 @@ class ApiLoginController extends AbstractController
             ref: "#/components/schemas/LoginUser"
         )
     )]
-    #[OA\Parameter(
-        name: "email",
-        description: "l'email de l'utilisateur",
-        in: "query",
-        required: true,
-        schema: new OA\Schema(type: "string")
-    )]
-    #[OA\Parameter(
-        name: "password",
-        description: "le mot de passe (en clair) de l'utilisateur",
-        in: "query",
-        required: true,
-        schema: new OA\Schema(type: "string")
-    )]
-    #[OA\RequestBody(
-        content: new OA\JsonContent(ref: "#/components/schemas/UserNew")
-    )]
+    /**
+     *     @OAn\RequestBody(
+     *         description="Username and password of the user to log in",
+     *         @OAn\MediaType(
+     *             mediaType="application/json",
+     *             @OAn\Schema(
+     *                 type="object",
+     *                 @OAn\Property(
+     *                     property="username",
+     *                     description="The user's unique username",
+     *                     type="string",
+     *                 ),
+     *                 @OAn\Property(
+     *                     property="password",
+     *                     description="The user's password",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     )
+     * */
     #[Route('/api/login', name: 'app_api_login', methods: ['POST'])]
     public function index(#[CurrentUser] ?User $user, UserRepository $repository)
     {
         if (null === $user) {
             return $this->json([
-               'message' => 'missing credentials',
+                'message' => 'missing credentials',
             ], Response::HTTP_UNAUTHORIZED);
         }
         $token = uniqid();
@@ -70,6 +75,13 @@ class ApiLoginController extends AbstractController
         content: new OA\JsonContent(
             ref: "#/components/schemas/LogoutUser"
         )
+    )]
+    #[OA\Parameter(
+        name: "Authorization",
+        description: "le token de l'utilisateur",
+        in: "header",
+        required: true,
+        schema: new OA\Schema(type: "string")
     )]
     #[IsGranted("ROLE_USER")]
     #[Security(name: "Bearer")]
