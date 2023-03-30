813aad386b4ebb3749bfee7a79ef55fa363eea0b
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2023-03-30 18:33:17 +0200

    Add pagination parameter

--- front/src/routes/FriendPage.js
+++ front/src/routes/FriendPage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import FriendList from "../components/FriendList";
 
 import Paginator from "../components/Paginator";
@@ -11,76 +11,81 @@ function FriendPage() {
     const navigate = useNavigate();
     const userId = localStorage.getItem("userId");
 
+    const [searchParams] = useSearchParams();
+
     const [friends, setFriends] = useState([]);
     const [friendsSuggestion, setFriendsSuggestion] = useState([]);
 
-    if (!userId){
+    if (!userId) {
         navigate("/");
     }
 
     useEffect(() => {
-        getFriend();
+        getFriend(searchParams);
         getSuggestFriends();
     }, [])
 
-    function getFriend () {
-        let serverQuery = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/api/user/${userId}/friends`;
+    function getFriend(params) {
+        let page = params.get('page') ? "?page=" + params.get('page') : "?page=1";
+        let nbResult = params.get('result') ? "&result=" + params.get('result') : "&result=4";
+        let serverQuery = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/api/user/${userId}/friends${page}${nbResult}`;
+        console.log(serverQuery);
         axios
-        .get(serverQuery)
-        .then(response => {
-            console.log(response);
-            let tmp = [];
-            for (let friend of response.data.data){
-                let data = {
-                    "id": friend.id,
-                    "fname": friend.USRFirstName,
-                    "name": friend.USRName,
-                    "img": friend.USRProfilePicture
+            .get(serverQuery)
+            .then(response => {
+                console.log(response);
+                let tmp = [];
+                for (let friend of response.data.data) {
+                    let data = {
+                        "id": friend.id,
+                        "fname": friend.USRFirstName,
+                        "name": friend.USRName,
+                        "img": friend.USRProfilePicture
+                    }
+                    tmp.push(data);
                 }
-                tmp.push(data);
-            }
-            setFriends({'nbResult': response.data.nbResult ,'data': tmp});
-        })
-        .catch(error => {
-            console.log(error.response.status);
-        })
+                setFriends({ 'nbResult': response.data.nbResult, 'data': tmp });
+            })
+            .catch(error => {
+                console.log(error.response.status);
+            })
     }
 
-    function getSuggestFriends () {
+    function getSuggestFriends() {
         let serverQuery = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/api/user/${userId}/recommendedusers`;
         axios
-        .get(serverQuery)
-        .then(response => {
-            let tmp = [];
-            for (let friend of response.data){
-                let data = {
-                    "id": friend.id,
-                    "fname": friend.USRFirstName,
-                    "name": friend.USRName,
-                    "img": friend.USRProfilePicture
+            .get(serverQuery)
+            .then(response => {
+                let tmp = [];
+                for (let friend of response.data) {
+                    let data = {
+                        "id": friend.id,
+                        "fname": friend.USRFirstName,
+                        "name": friend.USRName,
+                        "img": friend.USRProfilePicture
+                    }
+                    tmp.push(data);
                 }
-                tmp.push(data);
-            }
-            setFriendsSuggestion(tmp);
-        })
-        .catch(error => {
-            console.log(error.response.status);
-        })
+                setFriendsSuggestion(tmp);
+            })
+            .catch(error => {
+                console.log(error.response.status);
+            })
     }
 
-    return(
+    return (
         <div className="m-3">
             <p className="text-2xl text-iut-green">Amis</p>
-            <FriendList 
-            name={"Mes amis"}
-            following={true}
-            friends={friends.data ? friends.data : []}/>
-            <Paginator 
-            nbResult={friends.nbResult ? friends.nbResult : 0}/>
-            <FriendList 
-            name={"Recommendations"}
-            following={false}
-            friends={friendsSuggestion}/>
+            <FriendList
+                name={"Mes amis"}
+                following={true}
+                friends={friends.data ? friends.data : []} />
+            <Paginator
+                nbResult={friends.nbResult ? friends.nbResult : 0} />
+            <FriendList
+                name={"Recommendations"}
+                following={false}
+                friends={friendsSuggestion} />
         </div>
     )
 }
