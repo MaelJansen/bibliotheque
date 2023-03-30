e56a7e0e2980b39fa593359ddecd2a46829bf907
Mael Jansen <maeljansen2003@gmail.com>
2023-03-30 16:37:04 +0200

    Add logout function

--- back/src/Controller/ApiLoginController.php
+++ back/src/Controller/ApiLoginController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'app_api_login')]
-    public function index(#[CurrentUser] ?User $user)
+    public function index(#[CurrentUser] ?User $user, UserRepository $repository)
     {
         if (null === $user) {
             return $this->json([
@@ -19,10 +21,24 @@ class ApiLoginController extends AbstractController
             ], Response::HTTP_UNAUTHORIZED);
         }
         $token = uniqid();
+        $user->setToken($token);
+        $repository->save($user, true);
         return $this->json([
             'user'  => $user->getUserIdentifier(),
             'userId' => $user->getId(),
             'token' => $token,
         ]);
+    }
+
+    #[IsGranted("ROLE_USER")]
+    #[Security(name: "Bearer")]
+    #[Route('/api/logout', methods: ['POST'])]
+    public function logout(UserRepository $userRepository, Request $request){
+        $token = $request->headers->get('Authorization');
+        $token = substr($token, 7);
+        $user = $userRepository->getUserByToken($token);
+        $user->setToken(NULL);
+        $userRepository->save($user, true);
+        return $this->json(['userID' => $user->getId()]);
     }
 }
